6ea8ec9b09bf7fdd4f5d59f25d8419e7337c339e
Duncan <duncan.cornish.1999@gmail.com>
2022-08-24 14:22:15 +0100

    create result arrays

--- server/app.js
+++ server/app.js
@@ -4,28 +4,45 @@ const app = express()
 app.use(express.json())
 app.use(cors());
 
-const phResults = [
-    ["result1"],
-    ["result2"],
-    ["result3"]
+const foodResults = [
+    {id: 1, url: "", title: "result1", description: "", tags: "",},
+    {id: 2, url: "", title: "result2", description: "", tags: "",},
+    {id: 3, url: "", title: "result3", description: "", tags: "",}
+]
+
+const sportResults = [
+  {id: 1, url: "", title: "result1", description: "", tags: "",},
+  {id: 2, url: "", title: "result2", description: "", tags: "",},
+  {id: 3, url: "", title: "result3", description: "", tags: "",}
+]
+
+const filmResults = [
+    {id: 1, url: "", title: "result1", description: "", tags: "",},
+    {id: 2, url: "", title: "result2", description: "", tags: "",},
+    {id: 3, url: "", title: "result3", description: "", tags: "",}
 ]
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/results', (req, res) => res.json(phResults))
+app.get('/food', (req, res) => res.json(foodResults))
 
-app.get('/results/:id', (req, res) => {
-    try {
-        if (phResults[req.params.id-1]) {
-          res.send(phResults[req.params.id-1])
-        } else {
-          throw new Error('No result found')
-        }
+app.get('/sport', (req, res) => res.json(sportResults))
+
+app.get('/film', (req, res) => res.json(filmResults))
+
+
+// app.get('/results/:id', (req, res) => {
+//     try {
+//         if (phResults[req.params.id-1]) {
+//           res.send(phResults[req.params.id-1])
+//         } else {
+//           throw new Error('No result found')
+//         }
         
-    } catch (error) {
-        res.status(404).send({ message: error.message })
-    }
-  })
+//     } catch (error) {
+//         res.status(404).send({ message: error.message })
+//     }
+//   })
 
 const port = 3000
 app.listen(port, () => {
