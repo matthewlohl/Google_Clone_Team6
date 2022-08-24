a0eddba967788d107b53d4fec9b5546d4737dc68
Duncan <duncan.cornish.1999@gmail.com>
2022-08-24 14:26:43 +0100

    populate foodResults array

--- server/app.js
+++ server/app.js
@@ -5,9 +5,9 @@ app.use(express.json())
 app.use(cors());
 
 const foodResults = [
-    {id: 1, url: "", title: "result1", description: "", tags: "",},
-    {id: 2, url: "", title: "result2", description: "", tags: "",},
-    {id: 3, url: "", title: "result3", description: "", tags: "",}
+    {id: 1, url: "https://www.just-eat.co.uk", title: "Just Eat: Order takeaway online from 30000+ food delivery ...", description: "Order from local restaurants and takeaways online with Just Eat, the UK's leader in food delivery with over 30000+ restaurant menus offering Pizza, Chinese, ...", tags: "",},
+    {id: 2, url: "https://en.wikipedia.org/wiki/Food", title: "Food - Wikipedia", description: "Food is any substance consumed to provide nutritional support for an organism. Food is usually of plant, animal, or fungal origin, and contains essential ...", tags: "",},
+    {id: 3, url: "https://www.food.gov.uk/", title: "Food Standards Agency: Homepage", description: "Food Standards Agency makes sure food is safe and what it says it is.", tags: "",}
 ]
 
 const sportResults = [
