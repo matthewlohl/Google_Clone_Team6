b22884ad777041f4bbcc91c8c3af1b7ad345fd48
Duncan <duncan.cornish.1999@gmail.com>
2022-08-24 10:55:57 +0100

    add route to get specific results

--- server/app.js
+++ server/app.js
@@ -14,6 +14,18 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/results', (req, res) => res.json(phResults))
 
+app.get('/results/:id', (req, res) => {
+    try {
+        if (phResults[req.params.id-1]) {
+          res.send(phResults[req.params.id-1])
+        } else {
+          throw new Error('No result found')
+        }
+        
+    } catch (error) {
+        res.status(404).send({ message: error.message })
+    }
+  })
 
 const port = 3000
 app.listen(port, () => {
