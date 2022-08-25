df0eeb4ecff542810e9bcb85777c18f8e0b548a5
Matthew Lo <matthewlol316@gmail.com>
2022-08-25 10:41:44 +0100

    add env var, edit script and add procfile

--- server/app.js
+++ server/app.js
@@ -44,7 +44,7 @@ app.get('/film', (req, res) => res.json(filmResults))
 //     }
 //   })
 
-const port = 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`)
 })
